8f6e829f624be7c65a2ed3c16f3514f9adf64596
git  --version <jalynnking@gmail.com>
2022-06-20 12:23:53 -0400

    Create Discover component

---
+++ src/components/Discover.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {useState, useEffect} from 'react'
+function Discover({bookData}) {
+    const [selectedBooks, setSelectedBooks] = useState([])
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Discover;
