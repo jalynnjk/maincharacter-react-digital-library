5403b53b0bccf646e9e23f9614b2f479178b0556
git  --version <jalynnking@gmail.com>
2022-06-20 19:14:29 -0400

    Selected books display thumbnail and title when added to array

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
-function Discover({bookData}) {
-    const [selectedBooks, setSelectedBooks] = useState([])
-    return (
-        <div>
-            
-        </div>
-    );
+import { useState, useEffect } from 'react';
+function Discover({ selectedBooks }) {
+	console.log('this is the selected book', selectedBooks);
+	return (
+		<div>
+			{selectedBooks.map((selectedBook, index) => {
+				return(
+                <div className='selected-book-container'>
+                    <img src={selectedBook.volumeInfo.imageLinks.smallThumbnail} alt="" />
+                                    <h2 key={index}>{selectedBook.volumeInfo.title}</h2>
+                </div>)
+			})}
+		</div>
+	);
 }
 
 export default Discover;
