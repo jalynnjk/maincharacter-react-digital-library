60cebb1bdc7755019026e0b4af9a8458177bbe5a
git  --version <jalynnking@gmail.com>
2022-06-20 21:25:37 -0400

    Style the selected books list and add discover button

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -3,15 +3,18 @@ import { useState, useEffect } from 'react';
 function Discover({ selectedBooks }) {
 	console.log('this is the selected book', selectedBooks);
 	return (
-		<div>
-			{selectedBooks.map((selectedBook, index) => {
-				return(
-                <div className='selected-book-container'>
-                    <img src={selectedBook.volumeInfo.imageLinks.smallThumbnail} alt="" />
-                                    <h2 key={index}>{selectedBook.volumeInfo.title}</h2>
-                </div>)
-			})}
-		</div>
+		<div className='discover-container'>
+            <div className='selected-book-list-container'>
+                {selectedBooks.map((selectedBook, index) => {
+                    return(
+                            <div className='selected-book-container'>
+                                <img className='selected-book-img' src={selectedBook.volumeInfo.imageLinks.smallThumbnail} alt="" />
+                                                <h2 className='selected-book-title' key={index}>{selectedBook.volumeInfo.title}</h2>
+                            </div>)
+                })}
+                    </div>
+        <button>Discover</button>
+        </div>
 	);
 }
 
